0e3274e02420eec50af4081d3fad1119d2acef04
ifyaa <ifyaa@sina.com>
2020-11-08 22:33:50 +0900

    test-travice

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>여기는 어딘가 </h1>
+    <h1>여기는 어딘가-지랄입니다 </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
