1b94d769009e0360e68de1d2960137ca76ccb9fd
ifyaa <ifyaa@sina.com>
2020-11-15 01:07:01 +0900

    성공입니다 축카

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = ()=> {
     return (
         <Layout>
-                <h1>무슨지랄을 합니꺄ㅡ 벌써 며칠째인가 이게..</h1>
+                <h1>이게 성공할 줄 누가 알았나 </h1>
                 <h2>my page ---</h2>
             <Link to="/contact">contact me</Link>
         </Layout>
