68068827c0979e293206d873509ca28dd77441ed
ifyaa <ifyaa@sina.com>
2020-11-14 23:35:09 +0900

    99

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = ()=> {
     return (
         <Layout>
-                <h1>hello?</h1>
+                <h1>무슨지랄을 합니꺄ㅡ 벌써 며칠째인가 이게..</h1>
                 <h2>my page ---</h2>
             <Link to="/contact">contact me</Link>
         </Layout>
